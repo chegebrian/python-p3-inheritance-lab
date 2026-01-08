d94777ed7b7658f136bf8362cbbeaa0b327d8341
brianwaithaka <waithakabrian8@gmail.com>
2026-01-08 06:04:44 +0300

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,12 @@
 
 from user import User
 
+
 class Student(User):
-    
-    def learn(self):
-        pass
+
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge is not None else []
+
+    def learn(self, topic):
+        self.knowledge.append(topic)
